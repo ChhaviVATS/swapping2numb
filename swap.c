7feb5e20f4f482c40411a81f19a74ba1d73092c4
ChhaviVATS <99547112+ChhaviVATS@users.noreply.github.com>
2022-02-12 21:02:41 +0530

    swapping of two numbers
    this file prints swapping of two numbers on the screen

---
+++ swap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int a,b;
+  printf("input value for x and y:\n");
+  scanf("%d",&a);
+  scanf("%d",&b);
+  printf("before swapping the value of a and b:%d %d ,a,b);
+         a=a+b;
+         b=a-b;
+         a=a-b;
+         printf("after swapping the value of a and b :%d %d,a,b);
+                return 0;
+                }
